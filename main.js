b35af3ae1d9945051653a09f14691a72eae90bf9
NYAGO-MUREITHI <murraygoogz@gmail.com>
2019-12-16 20:01:32 +0300

    add description in main.html

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+var myIndex = 0;
+carousel();
+
+function carousel() {
+var i;
+var x = document.getElementsByClassName("mySlides");
+for (i = 0; i < x.length; i++) {
+  x[i].style.display = "none";
+}
+myIndex++;
+if (myIndex > x.length) {myIndex = 1}
+x[myIndex-1].style.display = "block";
+setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
 $(document).ready(function(){
   $("#empress").hover(function(){
     $("#empressp").slideDown()
@@ -24,4 +39,8 @@ $(document).ready(function(){
   },function(){
     $("#sjp").slideUp()
   });
+  $("#titles").click(function() {
+    $(".row").show()
+  })
+
 });
