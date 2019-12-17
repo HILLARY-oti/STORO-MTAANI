3d9cca5b64193198e9ee6cd70bfedeac7af6c06a
NYAGO-MUREITHI <murraygoogz@gmail.com>
2019-12-17 12:39:21 +0300

    add links to cloned branch

--- main.js
+++ main.js
@@ -13,6 +13,32 @@ x[myIndex-1].style.display = "block";
 setTimeout(carousel, 2000); // Change image every 2 seconds
 }
 
+
+function rating(d, t, e, m){
+
+// Async Rating-Widget initialization.
+window.RW_Async_Init = function(){
+
+    RW.init({
+        huid: "448334",
+        uid: "e9f53ab8f74a9b9a89846988dd54e2ea",
+        options: { "style": "oxygen" }
+    });
+    RW.render();
+};
+    // Append Rating-Widget JavaScript library.
+var rw, s = d.getElementsByTagName(e)[0], id = "rw-js",
+    l = d.location, ck = "Y" + t.getFullYear() +
+    "M" + t.getMonth() + "D" + t.getDate(), p = l.protocol,
+    f = ((l.search.indexOf("DBG=") > -1) ? "" : ".min"),
+    a = ("https:" == p ? "secure." + m + "js/" : "js." + m);
+if (d.getElementById(id)) return;
+rw = d.createElement(e);
+rw.id = id; rw.async = true; rw.type = "text/javascript";
+rw.src = p + "//" + a + "external" + f + ".js?ck=" + ck;
+s.parentNode.insertBefore(rw, s);
+}(document, new Date(), "script", "rating-widget.com/");
+
 $(document).ready(function(){
   $("#empress").hover(function(){
     $("#empressp").slideDown()
@@ -42,6 +68,7 @@ $(document).ready(function(){
   $("#titles").click(function() {
     $(".row").show()
   })
+  // fadeIn navbar and fadeOut
   $("#hamburger").click(function(){
     $(".topnav").fadeIn();
   });
