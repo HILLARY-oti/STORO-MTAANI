95192ab0ac7eb1b4c5d1ce7b28c1b4dc50d16e88
HILLARY-oti <hillaryotieno646@gmail.com>
2019-12-17 13:52:25 +0300

    add javascript function hide/show

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,18 @@ $(document).ready(function(){
     $("#songs").slideDown();
   });
 })
+$(document).ready(function () {
+  $("#close").click(function () {
+    $("#songs").slideUp();
+  });
+})
 $(document).ready(function(){
   $("#image").click(function(){
     $("#images").slideDown();
+  });
+});
+$(document).ready(function(){
+  $("#funga").click(function(){
+    $("#images").slideUp();
   });
 });
